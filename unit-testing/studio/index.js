6d84e681c890c19b185bd6c5a66099c8410a1ce7
unknown <jasiridickson@gmail.com>
2023-10-09 22:48:27 -0500

    Studio 10/9

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,6 +1,34 @@
 
 let launchcode = {
-  
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+
+  launchOutput: function(input){
+    if(input % 2 === 0 && input % 3 !== 0 && input % 5 !== 0){
+        return "Launch!";
+    }
+    if(input % 3 === 0 && input % 2 !== 0 && input % 5 !== 0){
+        return "Code!";
+    }
+    if(input % 5 === 0 && input % 3 !== 0 && input % 2 !== 0){
+        return "Rocks!";
+    }
+    if(input % 5 !== 0 && input % 3 === 0 && input % 2 === 0){
+        return "LaunchCode!";
+    }
+    if(input % 5 === 0 && input % 3 === 0 && input % 2 !== 0){
+        return "Code Rocks!";
+    }
+    if(input % 5 === 0 && input % 3 !== 0 && input % 2 === 0){
+        return "Launch Rocks! (CRASH!!!)";
+    }
+    if(input % 5 === 0 && input % 3 === 0 && input % 2 === 0){
+        return "LaunchCode Rocks!";
+    }
+  },
 }
 
 module.exports = launchcode;
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,53 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
+  test("should return true if the value of organization is `nonprofit`.", function(){
+    expect(launchcode.organization).toEqual("nonprofit");
+  })
   
+  test("should return true if the value of executiveDirector is `Jeff`.", function(){
+    expect(launchcode.executiveDirector).toEqual("Jeff");
+  })
+
+  test("should return true if percentageCoolEmployees is 100.", function(){
+    expect(launchcode.percentageCoolEmployees).toEqual(100);
+  })
+
+  test("should check if the programsOffered array is the correct size and containst the correct values.", function(){
+    expect(launchcode.programsOffered[0]).toEqual("Web Development");
+    expect(launchcode.programsOffered[1]).toEqual("Data Analysis");
+    expect(launchcode.programsOffered[2]).toEqual("Liftoff");
+
+    expect(launchcode.programsOffered.length).toEqual(3);
+  })
+
+  test("should check if the output of launchOutput() is divisible only by 2", function(){
+    expect(launchcode.launchOutput(2)).toEqual("Launch!");
+  })
+
+  test("should check if the output of launchOutput() is divisible only by 3", function(){
+    expect(launchcode.launchOutput(3)).toEqual("Code!");
+  })
+
+  test("should check if the output of launchOutput() is divisible only by 5", function(){
+    expect(launchcode.launchOutput(5)).toEqual("Rocks!");
+  })
+
+  test("should check if the output of launchOutput() is divisible by 2 and 3", function(){
+    expect(launchcode.launchOutput(6)).toEqual("LaunchCode!");
+  })
+
+  test("should check if the output of launchOutput() is divisible by 5 and 3", function(){
+    expect(launchcode.launchOutput(15)).toEqual("Code Rocks!");
+  })
+
+  test("should check if the output of launchOutput() is divisible by 2 and 5", function(){
+    expect(launchcode.launchOutput(10)).toEqual("Launch Rocks! (CRASH!!!)");
+  })
+
+  test("should check if the output of launchOutput() is divisible by 2, 3 and 5", function(){
+    expect(launchcode.launchOutput(30)).toEqual("LaunchCode Rocks!");
+  })
+
+
 });
